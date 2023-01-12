d511b282b09773988d4f904f57e053225d76cd1a
Partheeban Bharati <partheeban@Partheebans-MacBook-Air.local>
2023-01-12 11:03:54 -0800

    fix issue

--- index.js
+++ index.js
@@ -19,22 +19,29 @@ app.get('/api/v1/data', (req, res) => {
 });
 
 // handles adding new data
-app.post('/api/v1/add', async (req, res) => {
-    const sourceText = "INSERT INTO source (source_uri, source_title, source_date, source_note, source_belief, source_creator) VALUES($1, $2, $3, $4, $5, $6)";
+app.post('/api/v1/submit', async (req, res) => {
+    console.log('hello');
+
+
+    const sourceText = "INSERT INTO source (source_uri, source_title, source_date, source_note, source_belief, source_creator) VALUES($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING;";
     const sourceValues = [req.body.source_uri, req.body.source_title, req.body.source_date, req.body.source_note, req.body.source_belief, req.body.source_creator];
 
-    const valuePriceText = "INSERT INTO value_price (vp_num, curr_code, vp_date, vp_wtrunit, type_id, sector_code, source_uri, region_code, nat_code) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
-    const valuePriceValues = [req.body.vp_num, req.body.curr_code, req.body.vp_usd, req.body.vp_date, req.body.vp_wtrunit, req.body.type_id, req.body.sector_code, req.body.source_uri, req.body.region_code, req.body.nat_code];
+    const valuePriceText = "INSERT INTO value_price (vp_num, curr_code, vp_date, vp_wtrunit, type_id, sector_code, source_uri, region_code, nat_code) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+    const valuePriceValues = [req.body.price, req.body.currency, req.body.date, req.body.water_unit, req.body.price_type, req.body.user_sector, req.body.source_uri, req.body.region, req.body.country];
 
+    console.log('finished queries');
     try {
         await db.query(sourceText, sourceValues);
+        console.log('done source');
         await db.query(valuePriceText, valuePriceValues);
-
+        
+        console.log('success')
         res.status(201).json({
             status: "success"
         });
 
     } catch (err) {
+        console.log(err);
         res.status(500);
     }
 
@@ -43,11 +50,10 @@ app.post('/api/v1/add', async (req, res) => {
 // handles search queries
 app.post('/api/v1/query', async (req, res) => {
     try {
-
         // the  query and values are separate, where the nth item in the values array
         // corresponds to the nth item in the query
         // TODO refactor into utility function?
-        let query = "SELECT value_price.*, source.src_uri, source.src_title FROM value_price INNER JOIN source ON source.src_uri=value_price.source_uri WHERE";
+        let query = "SELECT value_price.*, source.source_uri, source.source_title FROM value_price INNER JOIN source ON source.source_uri=value_price.source_uri WHERE";
 
         const keys = Object.keys(req.body);
         const values = [];
@@ -60,9 +66,9 @@ app.post('/api/v1/query', async (req, res) => {
                 // TODO check if keys are valid to prevent SQL injection
                 // gotta be a better way to do this
                 if (nonEmptyKeys[i] === "earliest_year") {
-                    query = query.concat(" value_price." , "date", ` >= $${i + 1}`);
+                    query = query.concat(" value_price." , "vp_date", ` >= $${i + 1}`);
                 } else if (nonEmptyKeys[i] === "latest_year") {
-                    query = query.concat(" value_price." , "date", ` <= $${i + 1}`);
+                    query = query.concat(" value_price." , "vp_date", ` <= $${i + 1}`);
                 } else {
                     query = query.concat(" value_price." ,nonEmptyKeys[i], ` = $${i + 1}`);
                 };
@@ -75,13 +81,14 @@ app.post('/api/v1/query', async (req, res) => {
         };
 
         const results = await db.query(query, values);
-        
         res.status(201).json({
             status: "success",
             data: {
                 rows: results.rows
             }
         });
+
+
     } catch (err) {
         console.log(err);
     }})
